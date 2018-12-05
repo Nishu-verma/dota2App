f8d44beb1bb192f1270ccc1b0bd7e8f1df7816fd
Tyrone Rebolledo <t.rebolledo@hotmail.com>
2018-12-05 13:48:41 +1000

    Simple Player Data collected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dota 2 match Visualiser';
+  title = 'Dota 2 Web Application';
 }
--- src/app/player-details/player-details.component.ts
+++ src/app/player-details/player-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-player-details',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-details.component.css']
 })
 export class PlayerDetailsComponent implements OnInit {
-
-  constructor() { }
+  
+  playerId ="138354184"
+  playerResult : any;
+  playerMatches : any;
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    
   }
-
+  getPlayerData(){
+    this.http.get("https://api.opendota.com/api/players/"+this.playerId).subscribe((result)=>{
+      this.playerResult = result;
+      console.log(this.playerResult)
+    });
+    this.http.get("https://api.opendota.com/api/players/"+this.playerId+"/recentMatches").subscribe((pMatches)=>{
+      this.playerMatches = pMatches;
+      console.log(this.playerMatches)
+    });
+  }
 }
