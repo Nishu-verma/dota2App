12e28b1cd26f816a107a192e4eb96a9713bf0305
Tyrone Rebolledo <t.rebolledo@hotmail.com>
2019-01-10 00:33:31 +1000

    completed Basic chat bot functionality

--- src/app/hero-picker/hero-picker.component.ts
+++ src/app/hero-picker/hero-picker.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Location, KeyValue } from '@angular/common';
-// import * as AWS from 'aws-sdk'
+import {LexRuntime}  from 'aws-sdk';
+import {Message} from '../messages';
 
 
 @Component({
@@ -10,6 +11,8 @@ import { Location, KeyValue } from '@angular/common';
   templateUrl: './hero-picker.component.html',
   styleUrls: ['./hero-picker.component.css']
 })
+
+
 export class HeroPickerComponent implements OnInit {
 
   constructor(private http: HttpClient,
@@ -18,6 +21,10 @@ export class HeroPickerComponent implements OnInit {
 
   heroData: Array<any>[] = [];
   heroDataStored: any;
+  lex: LexRuntime;
+  userInput: string = "";
+  messages: Message[] = [];
+  lexState: string ="Hi what would you like to do";
 
   selectedHeroes: Array<any>[] = [];
   bannedHeroes: Array<any>[] = [];
@@ -26,11 +33,13 @@ export class HeroPickerComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroData();
+    this.messages.push(new Message(this.lexState,"Bot"));
+
   }
+  
 
   getHeroData() {
-    this.http.get("https://api.opendota.com/api/heroes")
-      .subscribe((result) => {
+    this.http.get("https://api.opendota.com/api/heroes").subscribe((result) => {
         this.heroData = result as Array<any>[];
         this.heroDataStored = result;
 
@@ -95,28 +104,39 @@ export class HeroPickerComponent implements OnInit {
     }
     );
   }
-  // postLexText() {
-  //   var lexruntime = new AWS.LexRuntime();
-  //   var params = {
-  //     botAlias: '\$LATEST', /* required */
-  //     botName: 'OrderFlowersBot', /* required */
-  //     inputText: 'Testing', /* required */
-  //     userId: 'User', /* required */
-  //     // requestAttributes: {
-  //     //   '<String>': 'STRING_VALUE',
-  //     //   /* '<String>': ... */
-  //     // },
-  //     // sessionAttributes: {
-  //     //   '<String>': 'STRING_VALUE',
-  //     //   /* '<String>': ... */
-  //     // }
-  //   };
-  //   lexruntime.postText(params, function(err, data) {
-  //     if (err) console.log(err, err.stack); // an error occurred
-  //     else     console.log(data);           // successful response
-  //   });
-  // }
-  // descOrder = (a, b) => {
-  //   if(a.value < b.value) return b.value;
-  // }
+  postLexText() {
+    var params = {
+      botAlias: '\$LATEST', /* required */
+      botName: 'OrderFlowersBot', /* required */
+      inputText: 'Testing', /* required */
+      userId: 'User', /* required */
+      // requestAttributes: {
+      //   '<String>': 'STRING_VALUE',
+      //   /* '<String>': ... */
+      // },
+      // sessionAttributes: {
+      //   '<String>': 'STRING_VALUE',
+      //   /* '<String>': ... */
+      // }
+    };
+
+    this.lex = new LexRuntime({
+      accessKeyId: "AKIAJ43PBK7346OYWSCQ",
+      secretAccessKey: "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",
+      region: "us-east-1"
+    }
+    );
+    params.inputText= this.userInput;
+    this.lex.postText(params, (err, data)=>{
+      if (err){
+        console.log(err, err.stack); // an error occurred
+      }
+      else {
+        console.log(data) // successful response
+        this.lexState = data.message;
+      }
+      this.messages.push(new Message(this.userInput,"User"));
+      this.messages.push(new Message(this.lexState,"Bot"));
+    });
+  }
 }
